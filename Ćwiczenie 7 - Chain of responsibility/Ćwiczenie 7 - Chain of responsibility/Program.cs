7b95160c9618dd8250119da00922db4915e6f0cf
KapitanSikMik <93821633+KapitanSikMik@users.noreply.github.com>
2024-05-25 12:44:30 +0200

    upload

---
+++ Ćwiczenie 7 - Chain of responsibility/Ćwiczenie 7 - Chain of responsibility/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Ćwiczenie_7___Chain_of_responsibility
+{
+    interface IHandler
+    {
+        void HandleRequest(int request);
+    }
+
+    class ConcreteHandlerTechnicalProblem : IHandler
+    {
+        private IHandler successor;
+         public void SetSuccessor (IHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public void HandleRequest (int request)
+        {
+            if (request == 1)
+            {
+                Console.WriteLine("Wybrano problem techniczny");
+            }
+            else
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreteHandlerBillingInquiry : IHandler
+    {
+        private IHandler successor;
+        public void SetSuccessor(IHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public void HandleRequest(int request)
+        {
+            if (request == 2)
+            {
+                Console.WriteLine("Wybrano zapytanie o rozliczenie");
+            }
+            else
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreteHandlerGeneralInquiry : IHandler
+    {
+        private IHandler successor;
+        public void SetSuccessor(IHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public void HandleRequest(int request)
+        {
+            if (request == 3)
+            {
+                Console.WriteLine("Wybrano zapytanie ogólne");
+            }
+            else
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var HandlerTechnicalProblem = new ConcreteHandlerTechnicalProblem();
+            var HandlerBillingInquiry = new ConcreteHandlerBillingInquiry();
+            var HandlerGeneralInquiry = new ConcreteHandlerGeneralInquiry();
+
+            HandlerTechnicalProblem.SetSuccessor(HandlerBillingInquiry);
+            HandlerBillingInquiry.SetSuccessor(HandlerGeneralInquiry);
+
+            var requests = new int[] { 1, 1, 2, 3, 2, 1 };
+            
+            foreach (var request in requests)
+            {
+                Console.WriteLine($"Processing request: {request}");
+                HandlerTechnicalProblem.HandleRequest(request);
+            }
+
+        }
+    }
+}
---
+++ Ćwiczenie 7 - Chain of responsibility/Ćwiczenie 7 - Chain of responsibility/Pseudokod.cs
@@ -0,0 +1,60 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Ćwiczenie_7___Chain_of_responsibility
+//{
+//    class Pseudokod
+//    {
+//        Interfejs Handler
+//    Metoda HandleRequest(żądanie)
+
+//Klasa ConcreteHandlerTechnicalProblem implementuje Handler
+//    Zmienna successor
+//    Metoda SetSuccessor(następca)
+//        Ustaw successor na następca
+
+//    Metoda HandleRequest(żądanie)
+//        Jeśli żądanie równe 1
+//            Wypisz "Wybrano problem techniczny"
+//        Inaczej
+//            Wywołaj HandleRequest na successor z żądaniem
+
+//Klasa ConcreteHandlerBillingInquiry implementuje Handler
+//    Zmienna successor
+//    Metoda SetSuccessor(następca)
+//        Ustaw successor na następca
+
+//    Metoda HandleRequest(żądanie)
+//        Jeśli żądanie równe 2
+//            Wypisz "Wybrano zapytanie o rozliczenie"
+//        Inaczej
+//            Wywołaj HandleRequest na successor z żądaniem
+
+//Klasa ConcreteHandlerGeneralInquiry implementuje Handler
+//    Zmienna successor
+//    Metoda SetSuccessor(następca)
+//        Ustaw successor na następca
+
+//    Metoda HandleRequest(żądanie)
+//        Jeśli żądanie równe 3
+//            Wypisz "Wybrano zapytanie ogólne"
+//        Inaczej
+//            Wywołaj HandleRequest na successor z żądaniem
+
+//Program główny
+//    Utwórz HandlerTechnicalProblem
+//    Utwórz HandlerBillingInquiry
+//    Utwórz HandlerGeneralInquiry
+
+//    Ustaw następce HandlerTechnicalProblem na HandlerBillingInquiry
+//    Ustaw następce HandlerBillingInquiry na HandlerGeneralInquiry
+
+//    Dla każdego żądania w liście żądań { 1, 1, 2, 3, 2, 1 }
+//    Wypisz "Przetwarzanie żądania: " + żądanie
+//    Wywołaj HandleRequest na HandlerTechnicalProblem z żądaniem
+
+//}
+//}
